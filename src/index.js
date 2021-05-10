cfc923d7cd901bf65d898fd622fe27eb153c5bc3
Ashwith Poojary <pashwith25@gmail.com>
2021-05-10 15:09:46 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('typingStart', msg => {
+  msg.send("Yoooooooooo");
+})
+
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
