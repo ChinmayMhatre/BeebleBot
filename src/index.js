7cd77c5e120413eb313926e9b607d122c4992a18
chinmay <chinmaymhatre12@gmail.com>
2021-05-12 21:53:43 +0530

    Added the commands file, created a simple command "ping"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 require('dotenv').config()
 const { Client } = require('discord.js');
 const client = new Client()
+const command = require("./commands");
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
+// client.on('message', msg => {
+//   if (msg.content === 'ping') {
+//     msg.reply('Pong!');
+//   }
+// });
+
+command(client,"ping",(message)=>{
+  message.channel.send("🏓Pong")
+})
 
 client.login(process.env.BOT_TOKEN);
