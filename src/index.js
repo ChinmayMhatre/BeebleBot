80312c9cd1efd21d1f8b34ae01869052a714757a
chinmay <chinmaymhatre12@gmail.com>
2021-05-12 22:07:36 +0530

    created command for server details

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const { Client } = require('discord.js');
 const client = new Client()
 const command = require("./commands");
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
 
 // client.on('message', msg => {
 //   if (msg.content === 'ping') {
@@ -13,8 +13,14 @@ client.on('ready', () => {
 //   }
 // });
 
-command(client,"ping",(message)=>{
-  message.channel.send("🏓Pong")
-})
+  command(client,"ping",(message)=>{
+    message.channel.send("🏓Pong")
+  })
+
+  command(client,"server",(message)=>{
+    client.guilds.cache.forEach((guild)=>{
+      message.channel.send(`Beeble Bot is part of  ${guild} and it has ${guild.memberCount} members`)
+    })
+  })
 
 client.login(process.env.BOT_TOKEN);
